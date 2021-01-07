5d397f9529a109a2e64e0048d1b5ad77992eb7b0
Matt Mulder <muldmatt@gmail.com>
2021-01-07 00:27:20 -0500

    refactor

--- README.md
+++ README.md
@@ -23,14 +23,5 @@ Then add this plugin to your Snowpack config:
 }
 ```
 
-### Plugin Options
-
-| Name        | Type                         | Description                                                                                                                   |
-| ----------- | ---------------------------- | ----------------------------------------------------------------------------------------------------------------------------- |
-| `path`      | `string`                     | (optional) The template path, as passed to `nunjucks.configure` (default: `process.cwd()`)                                    |
-| `opts`      | `string`                     | (optional) All other options accepted by `nunjucks.configure`.                                                                |
-| `extendEnv` | `(env: Environment) => void` | (optional) A callback that's provided the Nunjucks `Environment` before rendering, which lets you add filters and extensions. |
-
-
 [npm]: https://img.shields.io/npm/v/snowpack-plugin-nunjucks.svg
 [npm-url]: https://npmjs.com/package/snowpack-plugin-nunjucks
--- plugin.js
+++ plugin.js
@@ -2,41 +2,46 @@ const { promisify } = require('util');
 const nunjucks = require('nunjucks');
 const chokidar = require('chokidar');
 
-const render = promisify(nunjucks.render);
+const watchers = {};
 
-module.exports = function (snowpackConfig, options) {
+function watchDependencies({
+  env,
+  filePath,
+  onChange,
+}) {
+  if (!(filePath in watchers)) {
+    watchers[filePath] = chokidar.watch([]);
+    watchers[filePath].on('change', onChange);
+  }
+
+  env.on('load', (name) => {
+    if (name !== filePath) {
+      watchers[filePath].add(name);
+    }
+  });
+}
+
+module.exports = function () {
   return {
     name: 'snowpack-plugin-nunjucks',
     resolve: {
       input: ['.njk'],
       output: ['.html'],
     },
-    watchers: {},
     async load({ filePath, isDev }) {
-      const { path = process.cwd(), opts, extendEnv } = options;
-
-      const env = nunjucks.configure(path, opts);
+      const env = nunjucks.configure(process.cwd());
 
       if (isDev) {
-        if (!(filePath in this.watchers)) {
-          this.watchers[filePath] = chokidar.watch([]);
-          this.watchers[filePath].on('change', () => {
-            this.markChanged(filePath);
-          });
-        }
-
-        env.on('load', (name) => {
-          if (name !== filePath) {
-            this.watchers[filePath].add(name);
-          }
+        watchDependencies({
+          env,
+          filePath,
+          onChange: () => { this.markChanged(filePath) },
         });
       }
 
-      if (extendEnv) {
-        extendEnv(env);
-      }
-
-      return render(filePath);
+      return promisify(env.render.bind(env))(filePath);
     },
   };
 };
+
+module.exports.watchDependencies = watchDependencies;
